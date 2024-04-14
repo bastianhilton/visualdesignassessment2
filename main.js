a6384a4b817b9b911845fa076917912a9e143996
Sebastian hilton <bastianh86@outlook.com>
2024-04-14 15:15:25 +0100

    uploading without a framework

---
+++ main.js
@@ -0,0 +1,31 @@
+import Quote from "inspirational-quotes"; // NPM Module for inspirational quotes
+
+setInterval(myTimer, 1000); // set timer for clock to refresh
+
+// clock functionality for assessment 3
+function myTimer() {
+    // uses the months of the year to iterate through them
+    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+        "November", "December"
+    ];
+    const date = new Date(); // set javascript date object
+
+    let name = month[date.getMonth()]; // uses javascript month object
+    const day = new Date().toLocaleString(); // uses string to make the date easier to read
+
+    document.getElementById("dateTime").innerHTML = name + " " + day; // manipulates the DOM to show the date on the frontend
+
+    document.getElementById("quote").innerHTML = Quote.getRandomQuote();
+}
+
+setInterval(quoteTimer, 1000);
+
+function quoteTimer() {
+    document.getElementById("quote").innerHTML = Quote.getRandomQuote(); // gets a random quote from the above NPM module and then manipulates the DOM to show it on the frontend
+}
+
+// shows the current year in the footer
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById("year").innerHTML = currentYear;
